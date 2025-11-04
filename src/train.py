f7d2d325ba76053d586e1ae19170ef56bc8bb759
oxoxox-oxox <jiabaocao5@gmail.com>
2025-11-04 10:29:27 +0800

    初始提交：添加基础图像识别代码

---
+++ src/train.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from models.cnn_model import SimpleCNN
+import argparse
+
+def train_model():
+    # 参数设置
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=10)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--lr', type=float, default=0.001)
+    args = parser.parse_args()
+    
+    # 数据预处理
+    transform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    
+    # 加载数据集 (这里使用CIFAR-10作为示例)
+    train_dataset = datasets.CIFAR10(root='./data', train=True, 
+                                   download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, 
+                            shuffle=True)
+    
+    # 初始化模型
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = SimpleCNN(num_classes=10).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    
+    # 训练循环
+    for epoch in range(args.epochs):
+        model.train()
+        running_loss = 0.0
+        for i, (images, labels) in enumerate(train_loader):
+            images, labels = images.to(device), labels.to(device)
+            
+            optimizer.zero_grad()
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            running_loss += loss.item()
+            
+            if i % 100 == 99:
+                print(f'Epoch [{epoch+1}/{args.epochs}], '
+                      f'Step [{i+1}/{len(train_loader)}], '
+                      f'Loss: {running_loss/100:.4f}')
+                running_loss = 0.0
+    
+    # 保存模型
+    torch.save(model.state_dict(), 'model.pth')
+    print("训练完成！模型已保存为 model.pth")
+
+if __name__ == '__main__':
+    train_model()
